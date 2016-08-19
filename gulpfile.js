9e82b16d6c0d4006c5b2f8928188e06c4a17854f
Matt Shull <derekshull@gmail.com>
2016-08-19 15:03:27 -0500

    Added normalize and structure.

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 
 gulp.task('sass', function() {
-  return gulp.src('scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
+  return gulp.src('scss/*.scss') // Gets all files ending with .scss in app/scss and children dirs
     .pipe(sass())
     .pipe(gulp.dest('src/css/'))
     .pipe(browserSync.reload({
@@ -20,6 +20,6 @@ gulp.task('browserSync', function() {
 })
 
 gulp.task('watch', ['browserSync', 'sass'], function (){
-  	gulp.watch('scss/**/*.scss', ['sass']);
+  	gulp.watch('scss/*.scss', ['sass']);
 });
 
